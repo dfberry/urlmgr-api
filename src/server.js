67d0c14eb37375f82cb040fe11a4faaf80c33ce6
dfberry <dinaberry@outlook.com>
2017-02-24 13:41:45 -0800

    instanbul coverage

--- src/server.js
+++ src/server.js
@@ -9,6 +9,17 @@ var express = require('express'),
     users = require('./routes/users'),
     auth = require('./routes/authentication');
 
+// for coverage of apis
+var im = require('istanbul-middleware'),
+    isCoverageEnabled = (process.env.COVERAGE == "true"); 
+
+if (isCoverageEnabled) {
+    console.log('Hook loader for coverage - ensure this is not production!');
+    im.hookLoader(__dirname);
+        // cover all files except under node_modules 
+        // see API for other options 
+}
+
 var app = express();
 
 global.name = "help";
@@ -31,5 +42,11 @@ app.get("/", (req, res) => res.json({message: "Welcome to the app!"}));
 app.use('/v1/urls',urls);
 app.use('/v1/users',users);
 app.use('/v1/auth',auth);
+
+// test/coverage only
+if (isCoverageEnabled) {
+    //enable coverage endpoints under /coverage 
+    app.use('/coverage', im.createHandler());
+}
 
 module.exports = app;
