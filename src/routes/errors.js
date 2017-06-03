f0b062d4ec551c7afc8c74ff2c8b07fae5ac753a
dfberry <dinaberry@outlook.com>
2017-06-03 06:26:25 -0700

    fix name of func

--- src/routes/errors.js
+++ src/routes/errors.js
@@ -20,7 +20,7 @@ function errorHandler(err, req, res, next) {
     });
   } else if (err.message.indexOf("AuthFailure") >= 0){
 
-    libResponse.buildFailureSuccess(req, {url: req.url, api: req.path, method: req.method, error: { type: "auth", msg: err.message, stack: errorStack}}, {}, {}).then( json => {
+    libResponse.buildResponseFailure(req, {url: req.url, api: req.path, method: req.method, error: { type: "auth", msg: err.message, stack: errorStack}}, {}, {}).then( json => {
         res.status(422).json(json);
     }).catch(err => {
         res.status(500).json(err);
