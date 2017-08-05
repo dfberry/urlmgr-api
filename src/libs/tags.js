e255a548edf71ffe08d1f205bd71d56131afd317
dfberry <dinaberry@outlook.com>
2017-08-05 09:41:55 -0700

    tags return correct aggregation

--- src/libs/tags.js
+++ src/libs/tags.js
@@ -1,47 +1,43 @@
+ /*eslint-env mocha */
 "use strict";
 
 // each url has array of tags so need the url model
 const UrlModel = require('../data/url.js'),
-  _ = require('underscore');
+  Promise = require("bluebird");
 
 
 let Tags = {
 
   getByUserId: function(userUuid) {
-    let self = this;
     return new Promise(function(resolve, reject) {
 
       if(!userUuid) reject("tag by user id, user id is empty");
 
-      let query = '{$match:{ "userUuid" : "' + userUuid + '", tags: { $gt: [] }}},{$project:{_id:0, tags:1}},{$unwind: "$tags"},{$group: {_id:"$tags", count:{$sum:1}}},{$project:{_id:0,tag:"$_id", count:1}},{$sort: {tag:1}}';
+      let query = [{$match:{ "userUuid" : "' + userUuid + '", tags: { $gt: [] }}},{$project:{_id:0, tags:1}},{$unwind: "$tags"},{$group: {_id:"$tags", count:{$sum:1}}},{$project:{_id:0,tag:"$_id", count:1}},{$sort: {tag:1}}];
 
       UrlModel.findOne(query, (err, tagAggregation) =>{
         if(err)reject(err);
-        resolve(self.createReturnableTagArray(tagAggregation));
+        resolve(tagAggregation);
       });
     });
   },
   getAll: function(){
-    let self = this;
     return new Promise(function(resolve, reject) {
       
-      let query = '{$match:{ tags: { $gt: [] }}},{$project:{_id:0, tags:1}},{$unwind: "$tags"},{$group: {_id:"$tags", count:{$sum:1}}},{$project:{_id:0,tag:"$_id", count:1}},{$sort: {tag:1}}';
+      let query = [{$match:{ tags: { $gt: [] }}},{$project:{_id:0, tags:1}},{$unwind: "$tags"},{$group: {_id:"$tags", count:{$sum:1}}},{$project:{_id:0,tag:"$_id", count:1}},{$sort: {tag:1}}];
 
-      UrlModel.find(query, (err, tagAggregation) =>{
+      UrlModel.aggregate(query, (err, tagAggregation) =>{
         if(err)reject(err);
-        resolve(self.createReturnableTagArray(tagAggregation));
+        resolve(tagAggregation);
       });
     });
   },
-  createReturnableTagArray: function(tags){
-    if (!tags) return [];
-
-    return this.mungePropertyAndKeys(tags);
-  },
+  // DO NOT USE
+  // this doesn't work well for tags that start with non-chars such as '.net'
   mungePropertyAndKeys: function(arr){
     let jsonVariable = {};
     for(let i=1, len=arr.length; i < len; i++) {
-      jsonVariable[arr[i].tag] = arr[i].count;        
+      jsonVariable[(arr[i].tag).toString()] = arr[i].count;        
     }    
     return jsonVariable;
   }
