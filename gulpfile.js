eeb7ff267bed9329dee8176cf35eb067ad310f95
dfberry <dinaberry@outlook.com>
2017-09-07 20:35:47 -0700

    remove ts build from gulp, cleaned up final dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,20 @@
 const gulp = require('gulp');
-const eslint = require('gulp-eslint');
-const nodemon = require('gulp-nodemon');
-const mocha = require('gulp-mocha');
-const ts = require("gulp-typescript");
-let tsProject = ts.createProject("tsconfig.json");
 
-const MAIN_ENTRY = './src/index.js';
-const SRC_CODE = './src/**/*.js';
-const SRC_TEST = './src/**/*.spec.js';
-const IGNORE_CHANGES_ARRAY = [];
-
-// copy into dest folder
-const ASSETS = [
-    {src: './config.json',dest:'./dist/config/'},
-    {src: './src/public/*.*',dest:'./dist/public/'},
-    {src: './package.json',dest:'./dist'},
-    {src: './yarn.lock', dest:'./dist'}
-];
-
-const LINT_OPTIONS = {};
-
-gulp.task('lint', () => {
-    // ESLint ignores files with "node_modules" paths.
-    // So, it's best to have gulp ignore the directory as well.
-    // Also, Be sure to return the stream from the task;
-    // Otherwise, the task may end before the stream has finished.
-    return gulp.src([SRC_CODE])
-        // eslint() attaches the lint output to the "eslint" property
-        // of the file object so it can be used by other modules.
-        .pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
-        .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
-        .pipe(eslint.failAfterError());
-});
-
-gulp.task('develop', function () {
-  var stream = nodemon({ script: MAIN_ENTRY
-          , ext: 'html js'
-          , ignore: IGNORE_CHANGES_ARRAY
-          , tasks: ['lint'] 
-          , nodeArgs: ['--debug=5858']
-        })
- 
-  stream
-      .on('restart', function () {
-        console.log('restarted!')
-      })
-      .on('crash', function() {
-        console.error('Application has crashed!\n')
-         stream.emit('restart', 10)  // restart the server in 10 seconds 
-      })
-})
-
-gulp.task('test', () => 
-    gulp.src(SRC_TEST, {read: false})
-        // gulp-mocha needs filepaths so you can't have any plugins before it 
-        .pipe(mocha({reporter: 'nyan'}))
-);
 gulp.task('assets', function () {
-    // This will only run if the lint task is successful...
-        ASSETS.map((file) => {
-            gulp.src(file.src).pipe(gulp.dest(file.dest));
-        });
+
+        return gulp.src([
+            'src/**/*',
+            './package.json',
+            './yarn.lock',
+            '!./src/**/*.spec.*/',
+            '!./src/utilities/*.*/',
+            '!./src/utilities',
+            '!**/createUser.js/'
+          ])
+          .pipe(gulp.dest('dist'));
 });
 
-gulp.task("transpile", function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
-});
-
-gulp.task("build", ["assets", "transpile"], function () {
+gulp.task("build", ["assets"], function () {
     // This will only run if the lint task is successful...
     console.log("build is done");
-});
-
-//gulp.task('default', ['lint','test'], function () {
-gulp.task('default', ['copy', 'transpile'], function () {
-    // This will only run if the lint task is successful...
-    console.log("copy is done");
 });
